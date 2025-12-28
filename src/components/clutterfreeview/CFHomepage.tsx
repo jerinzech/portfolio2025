c58b62b1fe0c13b2f2675164208a8a04898b5616
jerinzech <jerinzdev@gmail.com>
2025-12-28 18:31:48 +0100

    added clutterfree homepage

---
+++ src/components/clutterfreeview/CFHomepage.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function CFHomepage() {
+  return (
+    <div className="w-1/2 h-[25vh]" >
+      <span style={{ fontWeight: 'bold' }}>Hey!</span> I’m <span style={{ fontWeight: 'bold' }}>Jerin James</span>.
+      < br />
+      I am a visual storyteller and strategist based in Grenoble, France, currently pursuing an MSc in Managing with Data and AI.
+      < br />
+      As an aspiring Product Manager and Data Strategist, I bridge the gap between technical AI concepts and tangible business value.My focus is on leveraging Data Analytics and Artificial Intelligence to build user - centric intelligent systems.
+      < br />
+      Here’s my LinkedIn, Github, and Gallery.
+    </div >
+  )
+}
+
+export default CFHomepage
