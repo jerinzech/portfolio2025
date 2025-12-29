746d51de0eb9c937a64bacd36ccb46ffb174c6e5
jerinzech <jerinzdev@gmail.com>
2025-12-29 15:07:17 +0100

    added border to check padding

--- src/components/clutterfreeview/CFHomepage.tsx
+++ src/components/clutterfreeview/CFHomepage.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function CFHomepage() {
   return (
-    <div className="CFHomepage-text flex-row items-center text-justify w-4/5 md:w-2/3 lg:w-[7/13] h-[25vh] text-[16px] lg:text-[24px] font-['Alexandria'] font-extralight" >
+    <div className="CFHomepage-text border flex-row items-center text-justify w-4/5 md:w-2/3 lg:w-[7/13] h-[25vh] text-[16px] lg:text-[24px] font-['Alexandria'] font-extralight" >
       <p><span className="text-[16px] lg:text-[25px]" style={{ fontWeight: 'bold' }}>Hey!!   </span>   I&apos;m <span style={{ fontWeight: 'bolder' }}>Jerin James</span> .</p>
       < br />
       <p>I am a <span style={{ fontWeight: 'bolder' }}>visual storyteller</span> and <span style={{ fontWeight: 'bolder' }}>strategist</span> based in <span style={{ fontWeight: 'bolder' }}>Grenoble, France</span>, currently pursuing an <span style={{ fontWeight: 'bolder' }}>MSc in Managing with Data and AI</span> .</p>
