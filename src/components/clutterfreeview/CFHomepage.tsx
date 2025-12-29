e63716404d96dfc0a2665ecf26e3d70ff00b21ac
jerinzech <jerinzdev@gmail.com>
2025-12-29 17:42:27 +0100

    corrected responsiveness

--- src/components/clutterfreeview/CFHomepage.tsx
+++ src/components/clutterfreeview/CFHomepage.tsx
@@ -2,15 +2,14 @@ import React from 'react'
 
 function CFHomepage() {
   return (
-    <div className="CFHomepage-text flex-row items-center text-justify w-4/5 md:w-2/3 lg:w-[7/13] h-min text-[16px] lg:text-[24px] font-['Alexandria'] font-extralight" >
-      <p><span className="text-[16px] lg:text-[25px]" style={{ fontWeight: 'bold' }}>Hey!!   </span>   I&apos;m <span style={{ fontWeight: 'bolder' }}>Jerin James</span> .</p>
+    <div className="CFHomepage-text flex-row items-center text-justify w-4/5 md:w-2/3 lg:w-[calc(100%*7/12)] h-min text-[15px] lg:text-[20px] font-['Alexandria'] font-extralight" >
+      <p><span className="text-[16px] lg:text-[22px]" style={{ fontWeight: 'bold', marginRight: '7px' }}>Hey!! </span> I&apos;m <span style={{ fontWeight: 'bolder' }}>Jerin James</span> .</p>
       < br />
-      <p>I am a <span style={{ fontWeight: 'bolder' }}>visual storyteller</span> and <span style={{ fontWeight: 'bolder' }}>strategist</span> based in <span style={{ fontWeight: 'bolder' }}>Grenoble, France</span>, currently pursuing an <span style={{ fontWeight: 'bolder' }}>MSc in Managing with Data and AI</span> .</p>
+      <p>I am a <span style={{ fontWeight: 'bolder' }}>visual storyteller</span> and <span style={{ fontWeight: 'bolder' }}>strategist</span> based in <span style={{ fontWeight: 'bolder' }}>Grenoble, France</span>, currently pursuing an <span style={{ fontWeight: 'bolder' }}>MSc in Managing with Data and Artificial Intelligence</span> .</p>
       < br />
       <p>As an aspiring <span style={{ fontWeight: 'bolder' }}>Product Manager</span> and <span style={{ fontWeight: 'bolder' }}>Data Strategist</span>, I bridge the gap between technical AI concepts and tangible business value . My focus is on leveraging Data Analytics and Artificial Intelligence to build user - centric intelligent systems .</p>
       < br />
       <p>Here&apos;s my <span style={{ fontWeight: 'bolder' }}>LinkedIn</span>, <span style={{ fontWeight: 'bolder' }}>Github</span>, <span style={{ fontWeight: 'bolder' }}>Resume</span>, and <span style={{ fontWeight: 'bolder' }}>Gallery</span> .</p>
-
     </div >
   )
 }
