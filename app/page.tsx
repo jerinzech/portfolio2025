4c49caf7dc53393975892c2fb82f291ef84b36b8
jerinzech <jerinzdev@gmail.com>
2025-12-29 20:04:26 +0100

    hide new cursor implementation

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   return (
 
     <>
-      <CustomCursor />
+      {/* <CustomCursor /> */}
       <div style={{
         backgroundImage: `linear-gradient(rgba(255, 255, 255, 0.03) 1px, transparent 1px), linear-gradient(90deg, rgba(255, 255, 255, 0.03) 1px, transparent 1px)`,
         backgroundSize: '20px 20px'
@@ -24,9 +24,9 @@ export default function Home() {
 
         <div className="homepage-body max-w-360 mx-auto min-h-[87vh] flex flex-col justify-center items-center">
           <CFHomepage />
-          <div className=""> <a href="#" className="px-6 py-3 bg-gray-200 rounded-lg text-xl text-black">
+          {/* <div className=""> <a href="#" className="px-6 py-3 bg-gray-200 rounded-lg text-xl text-black">
             Hover Me
-          </a></div>
+          </a></div> */}
         </div>
 
 
