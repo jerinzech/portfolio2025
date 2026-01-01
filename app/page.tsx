f8634d7342abec212cf2fc627db595270f6498c2
jerinzech <jerinzdev@gmail.com>
2026-01-02 00:58:28 +0100

    added noise

--- app/page.tsx
+++ app/page.tsx
@@ -6,29 +6,35 @@ export default function Home() {
   return (
 
     <>
-      {/* <CustomCursor /> */}
-      <div style={{
-        backgroundImage: `linear-gradient(rgba(255, 255, 255, 0.03) 1px, transparent 1px), linear-gradient(90deg, rgba(255, 255, 255, 0.03) 1px, transparent 1px)`,
-        backgroundSize: '20px 20px'
+      <div className="" style={{
+        backgroundImage: "url('/noiselayer.png')",
+        // backgroundRepeat: 'repeat',
+        backgroundSize: 'contain',
+        minHeight: '100vh',
       }}>
-        <div className="homepage-header max-w-360 mx-auto max-h-[10vh] ">
-          <Header />
-        </div>
+        <div style={{
+          backgroundImage: `linear-gradient(rgba(255, 255, 255, 0.09) 1px, transparent 1px), linear-gradient(90deg, rgba(255, 255, 255, 0.09)1px, transparent 1px)`,
+          backgroundSize: '20px 20px'
+        }}>
+          <div className="homepage-header max-w-360 mx-auto max-h-[10vh] ">
+            <Header />
+          </div>
 
-        {/*<div className="homepage-container border max-w-[1440px] mx-auto">
+          {/*<div className="homepage-container border max-w-[1440px] mx-auto">
         <BentoGridDemo />
       </div> */}
 
-        <div className="homepage-body max-w-360 mx-auto min-h-[87vh] flex flex-col justify-center items-center">
-          <CFHomepage />
-          {/* <div className=""> <a href="#" className="px-6 py-3 bg-gray-200 rounded-lg text-xl text-black">
+          <div className="homepage-body max-w-360 mx-auto min-h-[87vh] flex flex-col justify-center items-center">
+            <CFHomepage />
+            {/* <div className=""> <a href="#" className="px-6 py-3 bg-gray-200 rounded-lg text-xl text-black">
             Hover Me
           </a></div> */}
-        </div>
+          </div>
 
-        <div className="homepage-footer  max-w-360 mx-auto min-h-[3vh]">
-          <Footer />
-        </div>
+          <div className="homepage-footer  max-w-360 mx-auto min-h-[3vh]">
+            <Footer />
+          </div>
+        </div >
       </div >
     </>
 
