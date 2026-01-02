719ad4d5566341b4b21530c3b607bb8385e504b6
jerinzech <jerinzdev@gmail.com>
2026-01-02 01:12:13 +0100

    updated sizing

--- app/page.tsx
+++ app/page.tsx
@@ -6,36 +6,38 @@ export default function Home() {
   return (
 
     <>
-      <div className="" style={{
+
+      {/* <div className="" style={{
         backgroundImage: "url('/noiselayer.png')",
-        // backgroundRepeat: 'repeat',
-        backgroundSize: 'contain',
+        backgroundRepeat: 'repeat',
+        backgroundSize: 'justify',
         minHeight: '100vh',
+        zIndex: 10,
+
+      }}> */}
+
+      <div style={{
+        backgroundImage: `linear-gradient(rgba(255, 255, 255, 0.05) 1px, transparent 1px), linear-gradient(90deg, rgba(255, 255, 255, 0.05)1px, transparent 1px)`,
+        backgroundSize: '17px 17px'
       }}>
-        <div style={{
-          backgroundImage: `linear-gradient(rgba(255, 255, 255, 0.09) 1px, transparent 1px), linear-gradient(90deg, rgba(255, 255, 255, 0.09)1px, transparent 1px)`,
-          backgroundSize: '20px 20px'
-        }}>
-          <div className="homepage-header max-w-360 mx-auto max-h-[10vh] ">
-            <Header />
-          </div>
+        <div className="homepage-header max-w-360 mx-auto max-h-[10vh] ">
+          <Header />
+        </div>
 
-          {/*<div className="homepage-container border max-w-[1440px] mx-auto">
-        <BentoGridDemo />
-      </div> */}
 
-          <div className="homepage-body max-w-360 mx-auto min-h-[87vh] flex flex-col justify-center items-center">
-            <CFHomepage />
-            {/* <div className=""> <a href="#" className="px-6 py-3 bg-gray-200 rounded-lg text-xl text-black">
-            Hover Me
-          </a></div> */}
-          </div>
 
-          <div className="homepage-footer  max-w-360 mx-auto min-h-[3vh]">
-            <Footer />
-          </div>
-        </div >
+        <div className="homepage-body max-w-360 mx-auto min-h-[87vh] flex flex-col justify-center items-center">
+          <CFHomepage />
+        </div>
+
+        <div className="homepage-footer  max-w-360 mx-auto min-h-[3vh]">
+          <Footer />
+        </div>
       </div >
+
+
+
+      {/* </div > */}
     </>
 
 
